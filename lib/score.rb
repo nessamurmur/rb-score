43a73f096e27b677d0c4153cacb4c4d99bd7ce87
Nate West <natescott.west@gmail.com>
2014-05-20 14:04:59 -0500

    Rough score implementation

--- lib/score.rb
+++ lib/score.rb
@@ -1,5 +1,28 @@
-require "score/version"
+require 'json'
+require 'open-uri'
 
 module Score
-  # Your code goes here...
+
+  class Score
+    attr_reader :score
+
+    def initialize(url="https://github.com/databyte.json")
+      json = JSON.load(open(url))
+      @score = json.reduce(0) { |sum, j| sum + score_map.fetch(j["type"], 1) }
+    end
+
+    def to_s
+      score.to_s
+    end
+
+    def score_map
+      {
+        "PushEvent" => 5,
+        "PullRequestReviewCommentEvent" => 4,
+        "WatchEvent" => 3,
+        "CreateEvent" => 2,
+      }
+    end
+
+  end
 end
